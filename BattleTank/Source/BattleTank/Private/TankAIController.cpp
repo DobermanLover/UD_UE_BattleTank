7c5783997048a9f83696e1714a2e49a9b113e84e
LAPTOP-V1QKDJS1\Gene <dobermanlover65@charter.net>
2018-11-07 22:22:13 -0500

    commit thru lecture 131

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,6 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankAIController.h"
+#include "BattleTank.h"
+
+
+ATank* ATankAIController::GetAIControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto AIControlledTank = GetAIControlledTank();
+
+	///adding code to log problems with possessing a tank
+	if (AIControlledTank == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("AIPlayerController possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIPlayerController found on: %s"), *(AIControlledTank->GetName()));
+	}
+
+}
 
 
 
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "Tank.h"
 #include "CoreMinimal.h"
 #include "AIController.h"
 #include "TankAIController.generated.h"
@@ -14,7 +15,10 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
-	
+public:
+	ATank* GetAIControlledTank() const;
+
+	void BeginPlay() override;
 	
 	
 };
